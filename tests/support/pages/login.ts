f647de546779dba0ae897acf135ceb872ae3544d
Renan Oliveira <renan19922001@yahoo.com.br>
2025-03-20 14:59:47 -0300

    update

--- tests/support/pages/login.ts
+++ tests/support/pages/login.ts
@@ -1,5 +1,5 @@
 
-import { Locator, Page, expect } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 import { LoginModel } from "../../fixtures/login.model";
 
 export class LoginPage {
@@ -41,36 +41,5 @@ export class LoginPage {
     async getAlertText(){
         return await this.alertText.textContent()
     }
-
-    async toggle(taskName: string) {
-        const target = this.page.locator(`xpath=//p[text()="${taskName}"]/..//button[contains(@class, "Toggle")]`)
-        await target.click()
-    }
-
-    async remove(taskName: string) {
-        const target = this.page.locator(`xpath=//p[text()="${taskName}"]/..//button[contains(@class, "Delete")]`)
-        await target.click()
-    }
-
-    async shouldHaveText(taskName: string) {
-        const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
-        await expect(target).toBeVisible()
-    }
-
-    async shouldNotExist(taskName: string) {
-        const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
-        await expect(target).not.toBeVisible()
-    }
-
-    async alertHaveText(text: string) {
-        const target = this.page.locator('.swal2-html-container')
-        await expect(target).toHaveText(text)
-    }
-
-    async shouldBeDone(taskName: string) {
-        const target = this.page.getByText(taskName)
-        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
-    }
-
 
 }
